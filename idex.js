d607a25c8a9d8180a4101dc332dc0fdd27ad87b0
BoomslangRU <eilinals@mail.ru>
2021-08-09 20:27:11 +0300

    form processing

--- idex.js
+++ idex.js
@@ -1,11 +1,20 @@
 const express = require('express')
+const { urlencoded } = require('body-parser')
 
 const app = express()
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(urlencoded({ extended: true }))
 
 app.get('/', (request, response) => {
+    response.render('index')
+})
+
+app.post('/', (request, response) => {
+    const { city } = request.body
+
+    console.log(city);
     response.render('index')
 })
 
